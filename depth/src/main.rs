d99be99ee7522d8a5467288e351dbefc907ad8b9
lf-aaron <aaron@lakefieldcnc.com>
2022-08-25 11:40:41 -0400

    Fixed zmask

--- depth/src/main.rs
+++ depth/src/main.rs
@@ -4,11 +4,11 @@ use exr::prelude::*;
 use std::io::{BufWriter, Write};
 use std::fs;
 use std::path::{Path, PathBuf};
-use std::ops::Neg;
-use image::codecs::png;
-use image::codecs::png::{CompressionType, FilterType};
-use image::ColorType::Rgba8;
-use image::ImageEncoder;
+use std::ops::{Not};
+// use image::codecs::png;
+// use image::codecs::png::{CompressionType, FilterType};
+// use image::ColorType::{Rgba8, Rgb8};
+// use image::ImageEncoder;
 use webp;
 
 
@@ -18,9 +18,6 @@ struct CliArgs {
 
     #[clap(long)]
     resolution: u32,
-
-    #[clap(long)]
-    config: String,
 
     #[clap(long, parse(from_os_str))]
     zfront: PathBuf,
@@ -35,11 +32,11 @@ struct CliArgs {
     zplane: PathBuf,
 
     #[clap(long, parse(from_os_str))]
-    zrank: PathBuf,
+    zmask: PathBuf,
 }
 
 
-fn read_depth_exr(path: &Path) -> Vec<f32> {
+fn read_depth_exr(path: &Path, v: &mut Vec<f32>) {
     let channel = exr::prelude::read()
         .no_deep_data()
         .largest_resolution_level()
@@ -53,90 +50,145 @@ fn read_depth_exr(path: &Path) -> Vec<f32> {
         .list;
     
     match &channel.last().unwrap().sample_data {
-        exr::prelude::FlatSamples::F32(x) => x.to_owned(),
+        // exr::prelude::FlatSamples::F32(x) => v.splice(.., x.to_owned()),
+        exr::prelude::FlatSamples::F32(samples) => for (i, x) in samples.iter().enumerate() { v[i] = *x },
         _ => panic!("Unexpected channel type"),
-    }
+    };
 }
 
+fn depth_mask(frame: usize, z_front: &Vec<f32>, z_rear: &Vec<f32>, z_upper: &Vec<f32>, z_plane: &Vec<f32>, dims: &Dim4) -> Vec<u8> {
+    let batch = false;
+    let mask = constant::<bool>(true, dim4!(3, 3));
+    
+    let a_front = Array::new(z_front, *dims);
+    let a_rear = Array::new(z_rear, *dims);
+    let a_upper = Array::new(z_upper, *dims);
+    let a_plane = Array::new(z_plane, *dims);
 
-fn rank_depth(a: &Array<f32>) -> Vec<u8> {
-    let mask = arrayfire::constant::<f32>(1.0, arrayfire::Dim4::new(&[3, 3, 1, 1]));
-    let mut x = a.copy().neg();
-    x = arrayfire::dilate(&x, &mask);
-    let (_, i) = arrayfire::sort_index(&x, 2, false);
-    let mut buffer = vec!(0; a.dims().elements() as usize);
-    let casted_array = i.cast::<u8>();
-    let ordered_array = reorder_v2(&casted_array, 2, 0, Some(vec![1]));
-    ordered_array.host::<u8>(&mut buffer);
+    let f_r = lt(&a_front, &a_rear, batch);
+    let f_u = lt(&a_front, &a_upper, batch);
+    let r_u = lt(&a_rear, &a_upper, batch);
+    let r_f = lt(&a_rear, &a_front, batch);
+    let u_f = lt(&a_upper, &a_front, batch);
+    let u_r = lt(&a_upper, &a_rear, batch);
+
+    let mut m_front = f_r & f_u;
+    let mut m_rear = r_f & r_u;
+    let m_upper = u_f & u_r;
+
+    let r_p = match frame % 24 {
+        // Right side view
+        0 => {
+            let n = dims[0] as i32;
+            let p = gt(&range::<i32>(dim4!(n as u64), 0), &((n * 49/50)>>1), true);
+            and(&m_rear, &p, true)
+        },
+
+        // Facing away
+        1..=11 => {
+            let p = ge(&a_rear, &a_plane, true);
+            and(&m_rear, &p, batch)
+        },
+
+        // Left side view
+        12 => {
+            let n = dims[0] as i32;
+            let p = le(&range::<i32>(dim4!(n as u64), 0), &((n * 51/50)>>1), true);
+            and(&m_rear, &p, true)
+        },
+
+        // Facing toward
+        13..=23 => {
+            let p = lt(&a_rear, &a_plane, true);
+            and(&m_rear, &p, batch)
+        },
+        _ => panic!("This should never happen")
+    };
+
+    m_front = or(&m_front, &r_p, batch);
+    m_rear = and(&m_rear, &r_p.not(), batch);
+
+    let d_front = and(&dilate(&m_front, &mask), &lt(&or(&m_rear, &m_upper, batch), &1, true), batch);
+    let d_rear = and(&dilate(&m_rear, &mask), &lt(&or(&m_front, &m_upper, batch), &1, true), batch);
+    let d_upper = and(&dilate(&m_upper, &mask), &lt(&or(&m_front, &m_rear, batch), &1, true), batch);
+
+    let mut buffer = vec!(0; 3 * dims.elements() as usize);
+    // let mut ar = join_many![2; &m_front, &m_rear, &m_upper];
+    let mut ar = join_many![2; &d_front, &d_rear, &d_upper];
+    ar = reorder_v2(&ar, 2, 0, Some(vec![1]));
+    ar.cast::<u8>().host::<u8>(&mut buffer);
+
     return buffer;
 }
 
 
 fn save_webp(path: PathBuf, size: u32, pixels: &Vec<u8>) {
-    let img = webp::Encoder::from_rgba(pixels, size, size).encode_lossless();
+    let img = webp::Encoder::from_rgb(pixels, size, size).encode_lossless();
     let _ = fs::create_dir_all(path.clone().parent().unwrap());
     let mut buffered_file_write = BufWriter::new(fs::File::create(path).unwrap());
     buffered_file_write.write_all(&img).unwrap();
 }
 
-fn save_png(path: PathBuf, size: u32, pixels: &Vec<u8>) {
-    let buffered_file_write = &mut BufWriter::new(fs::File::create(path).unwrap());
-    png::PngEncoder::new_with_quality(
-        buffered_file_write,
-        CompressionType::Best,
-        FilterType::NoFilter
-    )
-    .write_image(
-        pixels,
-        size as u32,
-        size as u32,
-        Rgba8
-    )
-    .unwrap();
-}
+// fn save_png(path: PathBuf, size: u32, pixels: &Vec<u8>) {
+//     let buffered_file_write = &mut BufWriter::new(fs::File::create(path).unwrap());
+//     png::PngEncoder::new_with_quality(
+//         buffered_file_write,
+//         CompressionType::Best,
+//         FilterType::NoFilter
+//     )
+//     .write_image(
+//         pixels,
+//         size as u32,
+//         size as u32,
+//         Rgb8
+//     )
+//     .unwrap();
+// }
 
 fn main() {
     let args: CliArgs = CliArgs::parse();
     
     let size = args.resolution;
-    let config = &args.config;
     let zfront_dir = &args.zfront;
     let zrear_dir = &args.zrear;
     let zupper_dir = &args.zupper;
     let zplane_path = &args.zplane;
-    let zrank_dir = &args.zrank;
+    let zmask_dir = &args.zmask;
 
-    let zplane_files = fs::read_dir(zplane_path).unwrap().map(|f| f.unwrap()).collect::<Vec<fs::DirEntry>>();
     let zfront_files = fs::read_dir(zfront_dir).unwrap().map(|f| f.unwrap()).collect::<Vec<fs::DirEntry>>();
+    let zplane_files = fs::read_dir(zplane_path).unwrap().map(|f| f.unwrap()).collect::<Vec<fs::DirEntry>>();
     let zrear_files = fs::read_dir(zrear_dir).unwrap().map(|f| f.unwrap()).collect::<Vec<fs::DirEntry>>();
     let zupper_files = fs::read_dir(zupper_dir).unwrap().map(|f| f.unwrap()).collect::<Vec<fs::DirEntry>>();
 
     let num_frames = 144;
     if zfront_files.len() != num_frames { panic!("Missing 'Z Front' files"); }
+    if zplane_files.len() != num_frames { panic!("Missing 'Z Front' files"); }
     if zrear_files.len() != num_frames { panic!("Missing 'Z Rear' files"); }
     if zupper_files.len() != num_frames { panic!("Missing 'Z Upper' files"); }
 
-    let dims = Dim4::new(&[size as u64, size as u64, 4, 1]);
-    let mut array = vec![0_f32; size as usize * size as usize * 4];
-    // let zplane = read_depth_exr(&zplane_path);
+    let dims = dim4!(size as u64, size as u64);
+    let mut z_front = vec![0_f32; size as usize * size as usize];
+    let mut z_plane = vec![0_f32; size as usize * size as usize];
+    let mut z_rear = vec![0_f32; size as usize * size as usize];
+    let mut z_upper = vec![0_f32; size as usize * size as usize];
 
-    for i in 0..num_frames {
-        let f_zplane = &zplane_files[i];
-        let f_zfront = &zfront_files[i];
-        let f_zrear = &zrear_files[i];
-        let f_zupper = &zupper_files[i];
+    for frame in 0..num_frames {
 
-        // let zfront = read_depth_exr(&f_zfront.path());
-        // let zrear = read_depth_exr(&f_zrear.path());
-        // let zupper = read_depth_exr(&f_zupper.path());
-        array.splice(3*size as usize..4*size as usize, read_depth_exr(&f_zplane.path()));
-        array.splice(0*size as usize..1*size as usize, read_depth_exr(&f_zfront.path()));
-        array.splice(1*size as usize..2*size as usize, read_depth_exr(&f_zrear.path()));
-        array.splice(2*size as usize..3*size as usize, read_depth_exr(&f_zupper.path()));
+        let f_zplane = &zplane_files[frame];
+        let f_zfront = &zfront_files[frame];
+        let f_zrear = &zrear_files[frame];
+        let f_zupper = &zupper_files[frame];
 
-        let zrank = rank_depth(&Array::new(&array , dims));
+        read_depth_exr(&f_zfront.path(), &mut z_front);
+        read_depth_exr(&f_zplane.path(), &mut z_plane);
+        read_depth_exr(&f_zrear.path(), &mut z_rear);
+        read_depth_exr(&f_zupper.path(), &mut z_upper);
+        let zmask = depth_mask(frame, &z_front, &z_rear, &z_upper, &z_plane, &dims);
 
-        save_png(zrank_dir.join(format!("{:0>4}", (121 + i).to_string())).with_extension("png"), size, &zrank);
+        let path_out = zmask_dir.join(format!("{:0>4}", (121 + frame).to_string())).with_extension("webp");
+        save_webp(path_out, size, &zmask);
+        // let path_out = zmask_dir.join(format!("{:0>4}", (121 + frame).to_string())).with_extension("png");
+        // save_png(path_out, size, &zmask);
     }
 
 }
